c72fd3b1f07d4476b90e5cae2b3ea646fd4b6b8c
raghavjnvk <96766510+raghavjnvk@users.noreply.github.com>
2022-03-14 23:56:17 +0530

    Employee.java file
    Added Employee.java file

---
+++ Employee.java
@@ -0,0 +1,29 @@
+package Model;
+
+public class Employee {
+
+    private String firstName;
+    private String lastName;
+
+    public Employee(String firstName, String lastName) {
+        super();
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
